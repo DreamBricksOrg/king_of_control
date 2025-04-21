8c9284629ead5183269a7474065de211c22448dd
Ildefonso Zanette <izanette@gmail.com>
2025-04-21 10:31:57 -0300

    shows the frame and throws an exception when the calibration fails

--- king_of_control.py
+++ king_of_control.py
@@ -7,11 +7,15 @@ from dual_camera import DualCamera
 import time
 from cv2_utils import stack_frames_vertically
 
+
 class KingOfControl:
     def __init__(self):
+        print("Init cameras")
         self.cameras = DualCamera(cam1_id=param.CAMERA1_ID, cam2_id=param.CAMERA2_ID,
                                   res1=param.CAMERA_RESOLUTION, res2=param.CAMERA_RESOLUTION)
+        print("Init Arduino")
         self.board = HexagonsBoard(port=param.ARDUINO_COM_PORT, baudrate=param.ARDUINO_BAUD_RATE)
+        print("Init Board Model")
         self.hex_model_cam1 = HexBoardModel(param.HEXAGONS_SVG_FILE, center_offset=param.HEXAGONS_SVG_OFFSET)
         self.hex_model_cam2 = HexBoardModel(param.HEXAGONS_SVG_FILE, center_offset=param.HEXAGONS_SVG_OFFSET)
 
@@ -19,6 +23,7 @@ class KingOfControl:
         final_width = 640
         final_height = 720
         self.cameras.display(final_width, final_height)
+        cv2.destroyAllWindows()
 
     def calibration(self):
         yolo_object_detector = YoloObjectDetector(class_id=0, model_path=param.YOLO_MODEL_HEXAGON)
@@ -31,7 +36,7 @@ class KingOfControl:
         frame1, frame2 = self.cameras.get_frames()
         self.hex_model_cam1.draw_hexagons(frame1, color=(80, 80, 80))
         self.hex_model_cam2.draw_hexagons(frame2, color=(80, 80, 80))
-        composed_frame = stack_frames_vertically(frame1, frame2, param.CAMERA_RESOLUTION[0]/2, param.CAMERA_RESOLUTION[1])
+        composed_frame = stack_frames_vertically(frame1, frame2, 640, 720)
 
         if composed_frame is not None:
             cv2.imshow("Pressione espaco para continuar...", composed_frame)
@@ -54,6 +59,7 @@ class KingOfControl:
             coord = hex_cal_coord[coord_idx]
 
             # light up one hexagon
+            self.board.clear()
             self.board.set_hexagon(*coord, red)
             time.sleep(0.2)
             frame1, frame2 = self.cameras.get_frames()
@@ -63,8 +69,8 @@ class KingOfControl:
             if bbox1 is None:
                 num_exceptions1 += 1
                 if num_exceptions1 > 5:
-                    print("Erro na calibracao da camera 1")
-                    exit(1)
+                    self.show_frame(frame1)
+                    raise RuntimeError(f"Erro na calibracao da camera 1")
                 time.sleep(0.3)
                 continue
 
@@ -72,8 +78,8 @@ class KingOfControl:
             if bbox2 is None:
                 num_exceptions2 += 1
                 if num_exceptions2 > 5:
-                    print("Erro na calibracao da camera 2")
-                    exit(1)
+                    self.show_frame(frame2)
+                    raise RuntimeError(f"Erro na calibracao da camera 2")
                 time.sleep(0.3)
                 continue
 
@@ -84,9 +90,22 @@ class KingOfControl:
             num_exceptions1 = 0
             num_exceptions2 = 0
 
+        self.board.clear()
         floor_quad1 = self.hex_model_cam1.calculate_floor_quad(bboxes1)
         floor_quad2 = self.hex_model_cam1.calculate_floor_quad(bboxes2)
 
         return floor_quad1, floor_quad2
 
+    @staticmethod
+    def show_frame(frame, title="Pressione espaco para continuar..."):
+        while True:
+            cv2.imshow(title, frame)
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                cv2.destroyAllWindows()
+                break
 
+
+if __name__ == "__main__":
+    koc = KingOfControl();
+    koc.camera_setup()
+    koc.calibration()
