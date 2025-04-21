7424b78f51a176b4766ae0ebb8b592c74024347a
Ildefonso Zanette <izanette@gmail.com>
2025-04-21 18:55:57 -0300

    added parameter for the model to track the ball

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 
 CAMERA1_ID = 0
 CAMERA2_ID = 1
-CAMERA_RESOLUTION = (1280, 720)
+CAMERA_RESOLUTION = (640, 360)
 
 ARDUINO_COM_PORT = "COM16"
 ARDUINO_BAUD_RATE = 115200
@@ -10,3 +10,4 @@ HEXAGONS_SVG_FILE = r"static\assets\hexagons.svg"
 HEXAGONS_SVG_OFFSET = (-75.0, 150.0)
 
 YOLO_MODEL_HEXAGON = r"static\models\yolo11m_hexagon.pt"
+YOLO_MODEL_BALL = r"static\models\yolo11m.pt"
