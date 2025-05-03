ce0e55ffab91cde0a612d87e6df3cb4e7d9046ab
Ildefonso Zanette <izanette@gmail.com>
2025-05-03 11:54:49 -0300

    new test to check the YOLO model using both cameras

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
 import cv2
 from ultralytics import YOLO
+from dual_camera import DualCamera
+from cv2_utils import stack_frames_vertically
+
+YOLO_MODEL = r"static\models\yolo11m.pt"
 
 class Tests:
     def __init__(self,
@@ -44,7 +48,40 @@ class Tests:
             cap.release()
             cv2.destroyAllWindows()
 
+
+def test_yolo_dual_camera():
+
+    # Load YOLOv8 or YOLOv11 model
+    model = YOLO(YOLO_MODEL)  # You can replace with 'yolov11.pt' if you have it
+
+    # Open the webcam (change 0 to a different index if needed)
+    dual_cam = DualCamera(0, 1, (1280, 720), (1280, 720))
+
+    while True:
+        frame1, frame2 = dual_cam.get_frames()
+
+        # Run detection
+        results1 = model(frame1)
+        results2 = model(frame2)
+
+        # Draw detections using built-in plot() method
+        annotated_frame1 = results1[0].plot()
+        annotated_frame2 = results2[0].plot()
+
+        composed_frame = stack_frames_vertically(annotated_frame1, annotated_frame2, 640, 720)
+        cv2.imshow("Pressione espaco para continuar...", composed_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            cv2.destroyAllWindows()
+            break
+
+    # Clean up
+    cv2.destroyAllWindows()
+
+
 # Exemplo de uso:
 if __name__ == "__main__":
-    tests = Tests()
-    tests.ball_recognition_test()
+    #tests = Tests()
+    #tests.ball_recognition_test()
+
+    test_yolo_dual_camera()
