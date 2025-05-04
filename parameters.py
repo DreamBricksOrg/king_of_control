1fa257e350335a87eda8071fbaf7034808994e73
dreambricks <projeto@dreambricks.com.br>
2025-05-04 19:23:55 -0300

    using new model trained with the UCL ball

--- parameters.py
+++ parameters.py
@@ -10,15 +10,13 @@ HEXAGONS_SVG_FILE = r"static\assets\hexagons.svg"
 HEXAGONS_SVG_OFFSET = (-75.0, 150.0)
 
 YOLO_MODEL_HEXAGON = r"static\models\yolo11m_hexagon.pt"
-xYOLO_MODEL_BALL = r"static\models\yolo11m.pt"
-YOLO_MODEL_BALL = r"static\models\custom_ball.pt"
-xYOLO_MODEL_BALL_ID = 32
+YOLO_MODEL_BALL = r"static\models\ucl_custom_ball_v2.pt"
 YOLO_MODEL_BALL_ID = 0
 
 # game parameters
 MAX_TIME = 10    # in seconds
 COUNTDOWN_TIME = 4 # in seconds
-END_TIME = 5
+END_TIME = 8
 GOAL_TIME = 8
 OFFSIDE_TIME = 3
 
